49731f9082d989b1758bf21b70e4ebcb55944c4f
Julien NORMAND <julien.normand.92@gmail.com>
2016-04-18 22:02:20 +0200

    Create OS panel

--- ExportMobileAssets.jsx
+++ ExportMobileAssets.jsx
@@ -13,6 +13,106 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+var androidExport = [
+	{
+		name: "ldpi",
+		scaleFactor: 18.75,
+		type: "android"
+	},
+	{
+		name: "mdpi",
+		scaleFactor: 25,
+		type: "android"
+	},
+	{
+		name: "hdpi",
+		scaleFactor: 37.5,
+		type: "android"
+	},
+	{
+		name: "tvdpi",
+		scaleFactor: 43.75,
+		type: "android"
+	},
+	{
+		name: "xhdpi",
+		scaleFactor: 50,
+		type: "android"
+	},
+	{
+		name: "xxhdpi",
+		scaleFactor: 75,
+		type: "android"
+	},
+	{
+		name: "xxxhdpi",
+		scaleFactor: 100,
+		type: "android"
+	},
+	{
+		name: "web",
+		scaleFactor:266.667,
+		type: "android"
+	}
+];
+
+var iosExport = [
+	{
+		name: "@1x",
+		scaleFactor: 50,
+		type: "ios"
+	},
+	{
+		name: "@2x",
+		scaleFactor: 100,
+		type: "ios"
+	},
+	{
+		name: "@3x",
+		scaleFactor: 150,
+		type: "ios"
+	}
+];
+
+var windowsExport = [
+	{
+		name: "scale-80",
+		scaleFactor: 80,
+		type: "windows"
+	},
+		{
+		name: "scale-100",
+		scaleFactor: 100,
+		type: "windows"
+	},
+	{
+		name: "scale-125",
+		scaleFactor: 125,
+		type: "windows"
+	},
+	{
+		name: "scale-150",
+		scaleFactor: 150,
+		type: "windows"
+	},
+	{
+		name: "scale-180",
+		scaleFactor: 180,
+		type: "windows"
+	},
+	{
+		name: "scale-200",
+		scaleFactor: 200,
+		type: "windows"
+	},
+	{
+		name: "scale-400",
+		scaleFactor: 400,
+		type: "windows"
+	}
+];
+
+var selectedExport = {};
 
 var document = app.activeDocument;
 var folder = new Folder(document.path);
@@ -20,8 +120,17 @@ var folder = new Folder(document.path);
 if(document && folder) {
 	var dialog = new Window("dialog","Export assets to ...");
 
-	// ----- Folder destination
+	// ----- Folder panel
 	createFilePanel("File destination", dialog);
+	// -----
+
+	// ----- OS panel
+	var osGroup = dialog.add("group");
+	osGroup.orientation = 'row';
+	osGroup.alignChildren = 'top';
+	createOSPanel("Android", osGroup, androidExport);
+	createOSPanel("iOS", osGroup, iosExport);
+	createOSPanel("Windows", osGroup, windowsExport);
 	// -----
 
 	// ----- Button
@@ -50,6 +159,25 @@ function createFilePanel(name, parent) {
 			//alert("Cannot change current path");
 		}
 	};
+}
+
+function createOSPanel(name, parent, array) {
+	var panel = parent.add("panel", undefined, name);
+	panel.alignChildren = "left";
+
+	for(var i = 0; i < array.length;  i++) {
+		var cb = panel.add("checkbox", undefined, "\u00A0" + array[i].name);
+		cb.item = array[i];
+		cb.onClick = function () {
+			if (this.value) {
+				selectedExport[this.item.name] = this.item;
+				//alert("added: " + this.item.name + ", index: " + this.item.index);
+			} else {
+				delete selectedExport[this.item.name];
+				//alert("deleted: " + this.item.name + ", index: " + this.item.index);
+			}
+		};
+	}
 }
 
 function createButtonPanel(parent) {
